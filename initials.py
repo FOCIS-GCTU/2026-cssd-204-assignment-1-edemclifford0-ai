51afef640918ec8df7f4ceeb487b5977c5c2a8de
edemclifford0-ai <edemclifford0@gmail.com>
2026-05-30 11:59:52 +0000

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Clifford Edem Agbanyo
+# STUDENT ID:  2425402240
+# Email: 2425402240@gctu.edu.gh
+# Grader:  Mr. Augustus Buckman
+#
+# On my honor, Clifford Edem Agbanyo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+
+
+    print()
+    print("...CEA")
+    print()
+    print("...CCCCCCCCCCCC****...EEEEEEEEEEEE****...AAAAAAAAAAAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEEEEEEEEEEE****...AAAAAAAAAAAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCC         ****...EEE         ****...AAA      AAA****")
+    print("...CCCCCCCCCCCC****...EEEEEEEEEEEE****...AAA      AAA****")
+
+print()
+
+
+main()
